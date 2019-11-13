722ce08ff54cae8c6fb1a04a1835cba63dab11e0
Rafael Veloso <rafaelveloso11@gmail.com>
2019-11-12 22:14:01 -0300

    Add files via upload

---
+++ format.js
@@ -0,0 +1,9 @@
+$.datepicker.setDefaults({
+	dateFormat : 'dd/mm/yy',
+	changeYear : true,
+	changeMonth : true,
+	yearRange : 'c-50:c+50',
+	dayNamesMin : ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+	monthNamesShort : ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 
+	'Ago', 'Set', 'Out', 'Nov', 'Dez']
+});
